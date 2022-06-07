68a74a882d183beddf354c26fbb9bd909ecf9dd0
carpinx <colettamatias@gmail.com>
2022-06-07 15:24:35 -0300

    Responsive Product Detail page

--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 function Logo({width}) {
   return (
     <div className="logo">
-        <Link to={"/"}><img src="./img/logo.png" alt="Logo" width={width}/></Link>
+        <Link to={"/"}><img src="/img/logo.png" alt="Logo" width={width}/></Link>
     </div>
   )
 }
