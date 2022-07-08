4fc80532311f639de7e712cc4ccc841568929aa4
carpinx <colettamatias@gmail.com>
2022-07-08 13:04:46 -0300

    Show title and price only

--- src/components/search-results/Single.jsx
+++ src/components/search-results/Single.jsx
@@ -3,21 +3,12 @@ import { Link } from 'react-router-dom'
 
 function Single({product}) {
   return (
-    <Link to={`/items/${product.id}`} className="results__single">
-        <div className="image">
-            <img src={product.picture} alt={product.title} title={product.title} />
-        </div>
-        <div className="info">
-            <div className="info__priceAndShipping">
-                <h4 className="info__price">$ {product.price.amount.toLocaleString('es-AR')}</h4>
-                {product.free_shipping === true && <img src="img/free_shipping.png" alt="Envío gratis" title="Envío gratis" />}
-            </div>
-            <p className="info__title">{product.title}</p>
-        </div>
-        <div className="location">
-          {product.address}
-        </div>
-      </Link>
+      <table>
+        <tr>
+          <td>{product.title}</td>
+          <td>{product.price}</td>
+        </tr>
+      </table>
   )
 }
 
